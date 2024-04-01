37cd6c6ba64f9992a403efc27dffa7ebcdcf48f9
sick <siqueiraisrael96@gmail.com>
2024-03-31 20:11:35 -0400

    initial config

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+client = TestClient(app)
+
+
+def test_root_returns_ok_and_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Olá Mundo!'}
