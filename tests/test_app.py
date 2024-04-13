c828a33cc26d4c672b527ba2484649f3af1e1e21
sick <siqueiraisrael96@gmail.com>
2024-04-13 19:40:24 -0400

    test client fixture

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,27 +5,21 @@ from fast_zero.app import app
 client = TestClient(app)
 
 
-def test_root_returns_ok_and_hello_world():
-    client = TestClient(app)
-
+def test_root_returns_ok_and_hello_world(client):
     response = client.get('/')
 
     assert response.status_code == 200
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_html_returns_html():
-    client = TestClient(app)
-
+def test_html_returns_html(client):
     response = client.get('/html')
 
     assert response.status_code == 200
     assert response.text == 'Olá Mundo!'
 
 
-def test_create_user():
-    client = TestClient(app)
-
+def test_create_user(client):
     response = client.post(
         '/users/',
         json={
